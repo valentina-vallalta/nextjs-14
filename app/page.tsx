27b78f5928e8dfb3c1c670a371934fdfb33848ed
Thomas Gauvin <35609369+thomasgauvin@users.noreply.github.com>
2024-02-01 14:31:58 -0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div>
+        <h1>
+          Hello World 3
+        </h1>
+      </div>
+    </main>
+  );
+}
